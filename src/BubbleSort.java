3647236cb1cc09c07db63096dd38201eb76f3eea
gg29024 <gg29024@gmail.com>
2015-05-24 15:15:49 +0800

    7.16_3

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,25 +6,30 @@ public class BubbleSort {
 		
 		int temp;
 		int temp2;
+		
 		for (int i = 0; i < 6; i++){
-				if (m[i+1][0] < m[i][0]) {
-					temp = m[i+1][0];
-					m[i+1][0] = m[i][0];
+			for(int j = i+1; j < 6 ; j++){
+				if (m[j][0] < m[i][0]) {
+					temp = m[j][0];
+					m[j][0] = m[i][0];
 					m[i][0] = temp;
 					
-					temp2 = m[i+1][1];
-					m[i+1][1] = m[i][1];
+					temp2 = m[j][1];
+					m[j][1] = m[i][1];
 					m[i][1] = temp2;
 				} 
+			}
        	}
 		
 		for (int i = 0; i < 6; i++){
-			if (m[i+1][0] == m[i][0] && m[i+1][1] < m[i][1]) {
-				temp = m[i+1][1];
-				m[i+1][1] = m[i][1];
-				m[i][1] = temp;
+			for(int j = i+1; j < 6 ; j++){
+				if (m[j][0] == m[i][0] && m[j][1] < m[i][1]) {
+					temp = m[j][1];
+					m[j][1] = m[i][1];
+					m[i][1] = temp;
 				
-			} 
+				}
+			}
 		}
 	
 	}
@@ -33,6 +38,8 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		
 		int m[6][2] = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
+		
+		
 		
 		System.out.print(m[0][0]);
 	} 
