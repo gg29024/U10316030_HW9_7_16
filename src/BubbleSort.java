4cd779d24e96f87125a746a975694ce791037927
gg29024 <gg29024@gmail.com>
2015-05-24 16:51:16 +0800

    7.16_7

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,25 +2,27 @@
 
 
 public class BubbleSort {
+	//Method Bubble sort 
 	public static void sort(int m[][]) {
-		
+		//Data fields
 		int temp;
 		int temp2;
-		
+		//First time 
 		for (int i = 0; i < 6; i++){
 			for(int j = i+1; j < 6 ; j++){
 				if (m[j][0] < m[i][0]) {
+					//switch x
 					temp = m[j][0];
 					m[j][0] = m[i][0];
 					m[i][0] = temp;
-					
+					//switch y
 					temp2 = m[j][1];
 					m[j][1] = m[i][1];
 					m[i][1] = temp2;
 				} 
 			}
        	}
-		
+		//Second time
 		for (int i = 0; i < 6; i++){
 			for(int j = i+1; j < 6 ; j++){
 				if (m[j][0] == m[i][0] && m[j][1] < m[i][1]) {
@@ -36,7 +38,7 @@ public class BubbleSort {
 	
 	}
 	
-	
+	//Main method
 	public static void main(String[] args) {
 		
 		int m[][] = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
